19a2dbed69ad16bec6345e9b9c706add8698878b
Raj Thakur <rajt4900@gmail.com>
2024-04-10 11:28:47 +0530

    added EV in global.js

--- src/Utility/global.js
+++ src/Utility/global.js
@@ -1,4 +1,4 @@
-export const BASE_URL = `http://localhost:5000`
+export const BASE_URL = process.env.BASE_URL
 
 export const getCookie = (name) => {
     const cookieValue = document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)');
